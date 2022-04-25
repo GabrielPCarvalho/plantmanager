ada4796aeb7b1c0308858734bd045e071419b170
GabrielPCarvalho <gabriel57_pereira@hotmail.com>
2022-04-24 22:30:03 -0300

    Routes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from "react"; 
-import { UserIdentification } from "./src/pages/UserIdentification";
 import AppLoading from "expo-app-loading";
 import { 
   useFonts,
   Jost_400Regular, 
   Jost_600SemiBold
 } from "@expo-google-fonts/jost";
+import { Confirmation } from "./src/pages/Confirmation";
 
 export default function App(){
   const [ fontsLoaded ] = useFonts({
@@ -17,6 +17,6 @@ export default function App(){
     return <AppLoading />
     
   return (
-    <UserIdentification />
+    <Confirmation />
   )
 }
--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -16,41 +16,40 @@ export function Confirmation(){
         <View style={styles.container}>
             <View style={styles.content}>
                 <Text style={styles.emoji}>
-
+                    😊
                 </Text>
 
                 <Text style={styles.title}>
-                    
-
+                    Prontinho
                 </Text>
 
                 <Text style={styles.subtitle}>
                     Agora vamos começar a cuidar das suas
                     plantinhas com muito cuidado.
                 </Text>
+                <View style={styles.footer}>
+                    <Button />
+                </View>
             </View>
 
-            <View style={styles.footer}>
-                <Button />
-            </View>
         </View>
     )
 }
-
-// PAREI A AULA EM 1:11:23
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
-        justifyContent: "space-between"
+        justifyContent: "space-around"
     },
     content: {
-
-
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        width: '100%',
+        padding: 30
     },
     emoji: {
-        fontSize: 32,
+        fontSize: 78,
     },
     title: {
         fontSize: 22,
@@ -64,11 +63,12 @@ const styles = StyleSheet.create({
         fontFamily: fonts.text,
         textAlign: "center",
         fontSize: 17,
-        paddingVertical: 20,
+        paddingVertical: 10,
         color: colors.heading,
     },
     footer: {
         width: '100%',
-        paddingHorizontal: 75
+        paddingHorizontal: 50,
+        marginTop: 20
     }
 })
